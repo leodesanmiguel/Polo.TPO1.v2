3c21b5c14bbadbb73d448c04d0a7b968435431f2
Leo <leodesanmiguel@gmail.com>
2021-09-29 13:29:47 -0300

    Muestra la pantalla del TPO1 de Polo Version 2 usando el modelo que parece una web

---
+++ src/Logicas/Reglas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logicas;
+
+import Vistas.Agenda2;
+
+/**
+ *
+ * @author Leo Martinez
+ * 
+ */
+public class Reglas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Agenda2 ag = new Agenda2();
+        ag.setVisible(true);
+        ag.setLocationRelativeTo(null);
+        
+    }
+
+}
